8c22a5d42a306e39db6ed4af1edd29b0640d3b78
ANDRESVIDRIO <andresvidrio88@gmail.com>
2025-04-10 15:25:34 -0600

    Add files via upload

--- Analisis.py
+++ Analisis.py
@@ -5,7 +5,7 @@ import numpy as np
 import matplotlib.pyplot as plt
 import seaborn as sb
 from datetime import datetime, timedelta
-from google.genai import typ
+
 
 
 #############################################
